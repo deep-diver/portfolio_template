f5087a767af266d3be1d177b5aa023a6ad2e3bbd
deep-diver <deep.diver.csp@gmail.com>
2017-03-13 15:59:28 +0900

    divide controllers into View & Rest types.

---
+++ src/main/java/indi/csp/web/controllers/RestControllers.java
@@ -0,0 +1,8 @@
+package indi.csp.web.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllers {
+
+}
---
+++ src/main/java/indi/csp/web/controllers/ViewControllers.java
@@ -0,0 +1,8 @@
+package indi.csp.web.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ViewControllers {
+
+}
