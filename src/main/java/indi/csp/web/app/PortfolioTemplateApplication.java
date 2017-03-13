51a2c049336b05971500aa0d6e541a43a7fe460c
deep-diver <deep.diver.csp@gmail.com>
2017-03-13 14:32:31 +0900

    initial commit.

---
+++ src/main/java/indi/csp/web/app/PortfolioTemplateApplication.java
@@ -0,0 +1,12 @@
+package indi.csp.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTemplateApplication.class, args);
+	}
+}
