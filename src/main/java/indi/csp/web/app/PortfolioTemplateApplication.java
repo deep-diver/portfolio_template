f7dbed547fe1982f33434976f710b9da19a384f9
deep-diver <deep.diver.csp@gmail.com>
2017-04-04 20:17:45 +0900

    #19 basic security checkin. authenticated URI access role has been applied

--- src/main/java/indi/csp/web/app/PortfolioTemplateApplication.java
+++ src/main/java/indi/csp/web/app/PortfolioTemplateApplication.java
@@ -2,11 +2,18 @@ package indi.csp.web.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class PortfolioTemplateApplication {
+public class PortfolioTemplateApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioTemplateApplication.class, args);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/signin").setViewName("login");
 	}
 }
