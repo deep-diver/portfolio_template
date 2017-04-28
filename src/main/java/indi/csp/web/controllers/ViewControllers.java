c174cbcd26715281c614ccadc6c1692c17b8b4d6
deep-diver <deep.diver.csp@gmail.com>
2017-04-28 11:21:01 +0900

    #29 adding themeleaf for loop to display game list.

--- src/main/java/indi/csp/web/controllers/ViewControllers.java
+++ src/main/java/indi/csp/web/controllers/ViewControllers.java
@@ -1,14 +1,29 @@
 package indi.csp.web.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import indi.csp.web.database.VOMapper;
+import indi.csp.web.vo.ItemListVO;
+
 @Controller
 public class ViewControllers {
+	@Autowired
+	private VOMapper voMapper;	
+	
 	@RequestMapping("/")
 	public ModelAndView viewOfListOfItem() {
+		ItemListVO result = voMapper.getItemsByRange(0, 9);
+		
 		ModelAndView view = new ModelAndView();
+		view.setViewName("listContents");
+		
+		if (result.getTotalCount() > 0) {
+			view.addObject("item_list", result.getItems());
+		}
+		
 		return view;
 	}
 	
